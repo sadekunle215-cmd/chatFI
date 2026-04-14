10056d9a6d64fcd41d44a1ffe3875cd03d0ac3c3
sadekunle215-cmd <sadekunle215@gmail.com>
2026-04-14 08:11:23 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import JupChat from "./JupChat";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <JupChat />
+  </React.StrictMode>
+);
