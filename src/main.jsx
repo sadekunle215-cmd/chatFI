47b43cfef1ca155739767566a64669bc2f9d2026
sadekunle215-cmd <sadekunle215@gmail.com>
2026-04-30 08:40:56 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AppProviders from "./providers/AppProviders";
 import ChatFi from "./chatFi";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChatFi />
+    <AppProviders>
+      <ChatFi />
+    </AppProviders>
   </React.StrictMode>
 );
